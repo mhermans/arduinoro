039845548417164965ed341a603c3618447c3fae
Maarten Hermans <maarten.hermans@gmail.com>
2014-12-13 00:31:22 +0100

    check in basic examples

---
+++ examples/seven_seg_float/seven_seg_float.ino
@@ -0,0 +1,24 @@
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+//#include <TinyWireM.h> // Enable this line if using Adafruit Trinket, Gemma, etc.
+
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+void setup() {
+#ifndef __AVR_ATtiny85__
+  Serial.begin(9600);
+  Serial.println("7 Segment Backpack Test");
+#endif
+  matrix.begin(0x70);
+}
+
+void loop() {
+
+  // print a floating point 
+  matrix.print(12.34);
+  matrix.writeDisplay();
+  delay(500);
+  
+}
